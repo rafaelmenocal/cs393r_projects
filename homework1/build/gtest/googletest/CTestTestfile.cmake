29f4edd4cae1f7554154e52e87cabbeaae6d1608
Rafael Garcia-Menocal <r.garcia.menocal@gmail.com>
2021-09-03 12:05:45 -0500

    updated .gitignore

--- homework1/build/gtest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest/googletest
-# Build directory: /home/raf/Programming/cs393r/projects/cs393r_starter/build/gtest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
